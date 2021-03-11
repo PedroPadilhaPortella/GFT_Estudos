8099b34f8492fa1f4ea2590c50a46713fdee369e
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-03-11 20:28:41 -0300

    movendo os pacotes pra outro lugar

--- Desenvolvimento Avançado com Angular/MinhaApp/src/app/demos/arquitetura-componentes/produto.app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'produto-app-component',
-    template: '<router-outlet></router-outlet>'
-})
-export class ProdutoAppComponent {}
--- Desenvolvimento Avançado com Angular/MinhaApp/src/app/demos/bar-di-zones/bar.config.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export interface BarUnidadeConfig {
-    unidadeId: number,
-    unidadeToken: string
-}
-
-export const BAR_UNIDADE_CONFIG = new InjectionToken<BarUnidadeConfig>('BAR_UNIDADE_CONFIG')
